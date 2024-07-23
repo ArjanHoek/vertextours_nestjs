2edca9fe79da52c7a20cfb8cc37681cd80c42c0f
Arjan Hoek <arjanhoek5191@gmail.com>
2024-07-23 19:40:00 +0200

    add separate database module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,30 +1,15 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { RefugeModule } from './refuge/refuge.module';
 
-import { User } from '../entities/user.entity';
-import { Refuge } from '../entities/refuge.entity';
+import { DbModule } from './database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (conf: ConfigService) => ({
-        type: 'postgres',
-        host: conf.get('DB_HOST'),
-        port: +conf.get('DB_PORT'),
-        username: conf.get('DB_USER'),
-        password: conf.get('DB_PASS'),
-        database: conf.get('DB_NAME'),
-        entities: [User, Refuge],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
+    DbModule,
     AuthModule,
     UserModule,
     RefugeModule,
---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Refuge } from 'src/entities/refuge.entity';
+import { User } from 'src/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (conf: ConfigService) => ({
+        type: 'postgres',
+        host: conf.get('DB_HOST'),
+        port: +conf.get('DB_PORT'),
+        username: conf.get('DB_USER'),
+        password: conf.get('DB_PASS'),
+        database: conf.get('DB_NAME'),
+        entities: [User, Refuge],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DbModule {}
